aa5d9f6e4ae8126bef28e9f73ddadb52e659e55e
SungJun <psj12343@gmail.com>
2025-10-01 01:28:21 +0900

    10/01 First Commit

--- src/components/countContainer.tsx
+++ src/components/countContainer.tsx
@@ -6,11 +6,13 @@ function CountContainer() {
 
     const [count, setCount] = useState<Count>({num:10})
 
+    const p1: Person = {first:'길동', last:'홍'}
+
     return (
         <div className={'bg-amber-200 h-1/3 text-center pt-4 text text-4xl bold'}>
             COUNT CONTAINER
 
-            <CountDisplay count={count}></CountDisplay>
+            <CountDisplay count={count} person={p1}></CountDisplay>
             <CountButtons></CountButtons>
         </div>
     );
--- src/components/countDisplay.tsx
+++ src/components/countDisplay.tsx
@@ -1,8 +1,15 @@
 
-function CountDisplay({num}: Count) {
+interface CountDisplayProps {
+    count: Count
+}
+
+function CountDisplay({count}: {count:CountDisplayProps}) {
+
+    console.log(count)
+
     return (
         <div className={'text-4xl'}>
-            NUMBER: {num}
+            NUMBER:
         </div>
     );
 }
