a876be1f71f8a49a5b5c9723283c13620c2e6d65
SungJun <psj12343@gmail.com>
2025-10-01 22:42:58 +0900

    10/01 Sixth Commit

--- src/components/todo/todoContainer.tsx
+++ src/components/todo/todoContainer.tsx
@@ -1,18 +1,20 @@
-import {useState} from "react";
+import { useState} from "react";
 import { v4 as uuidv4 } from 'uuid';
+import TodoList from "./todoList.tsx";
+import TodoInput from "./todoInput.tsx";
 
 function TodoContainer() {
 
     const [todos, setTodos] = useState<Todo[]>([])
 
-    const addTodo = (title:string)=> {
+    const addTodo = (title: string) => {
 
-        const obj = {tid: uuidv4() , title:title}
+        const obj = {tid: uuidv4(), title: title}
 
-        setTodos([...todos,obj])
+        setTodos([...todos, obj])
     }
 
-    const removeTodo = (tid:string) => {
+    const removeTodo = (tid: string) => {
 
         const filteredResult =
             todos.filter(todo => todo.tid !== tid)
@@ -22,16 +24,15 @@ function TodoContainer() {
 
     console.log(addTodo, removeTodo)
 
+
+
     return (
         <div className={'w-full h-2/3 bg accent-fuchsia-400 border-1'}>
-            <div className={'m-2 p-2 h-1/5 flex justify-center'}>
-                <input type={'text'} className={'w-2/3 bg-white p-2 h-10'}/>
-                <button>SAVE</button>
-            </div>
-            <div>
-                <ul>
 
-                </ul>
+            <TodoInput addTodo={addTodo}></TodoInput>
+
+            <div>
+                <TodoList arr={todos} remove={removeTodo}></TodoList>
             </div>
 
         </div>
