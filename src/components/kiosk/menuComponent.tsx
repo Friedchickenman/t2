58c6090398573a82b0926a7225ad2855d838e085
SungJun <psj12343@gmail.com>
2025-10-03 20:05:31 +0900

    10/03 12th Commit

--- src/components/kiosk/menuComponent.tsx
+++ src/components/kiosk/menuComponent.tsx
@@ -9,30 +9,32 @@ interface MenuComponentProps {
 
 function MenuComponent( {menus, addCart} : MenuComponentProps) {
 
+    const [cat, setCat] = useState('A')
 
-    const [arr, setArr] = useState<Menu[]>(menus)
+    const showCategory = (cat:string):void => {
 
-    const showCategory = (cat:string) => {
-
-        if(cat === 'A') {
-            arr.forEach(menu => menu.show = true)
-        }else{
-            arr.forEach(menu=> {menu.show = menu.category === cat})
-        }
-        setArr([...arr])
-
+        setCat(cat)
     }
+
+    const targetMenus = menus.filter(menu=> {
+        if(cat === 'A'){
+            return true
+        }else{
+            return menu.category === cat
+        }
+    })
 
     const handleClick = (mno:number) => {
 
-        if(confirm('장바구니에 추가하시겠습니까?')) {
-            console.log(mno)
-        }
-
+    if(confirm('장바구니에 추가하시겠습니까?')) {
+        console.log(mno)
+        addCart(mno)
     }
 
-    const menuLis = arr.map( menu => menu.show ?
-        <div onClick={() => handleClick(menu.mno)} key={menu.mno} className="max-w-xs bg-white rounded-2xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105">
+}
+
+    const menuLis = targetMenus.map( menu => menu.show ?
+    <div onClick={() => handleClick(menu.mno)} key={menu.mno} className="max-w-xs bg-white rounded-2xl shadow-lg overflow-hidden transform transition duration-300 hover:scale-105">
         <img src={menu.imgName} alt={menu.name} className="w-full h-48 object-cover" />
         <div className="p-4 text-center">
             <h2 className="text-lg font-semibold text-gray-800">
@@ -44,32 +46,33 @@ function MenuComponent( {menus, addCart} : MenuComponentProps) {
         </div>
     </div>:<></>)
 
-    return (
+    return(
         <div>
-            <div>Menus</div>
-            <div className={'flex m-4 p-2'}>
-                <div
-                    className={'m-2 p-2 bg-amber-500'}
-                    onClick={() => showCategory('A')}
-                >All</div>
-                <div
-                    className={'m-2 p-2 bg-amber-500'}
-                    onClick={() => showCategory('C')}
-                >Coffee</div>
-                <div
-                    className={'m-2 p-2 bg-amber-500'}
-                    onClick={() => showCategory('D')}
-                >Deserts</div>
-                <div
-                    className={'m-2 p-2 bg-amber-500'}
-                    onClick={() => showCategory('G')}
-                >Goods</div>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-                {menuLis}
-            </div>
-        </div>
-    );
+                    <div>Menus</div>
+                    <div className={'flex m-4 p-2'}>
+                        <div
+                            className={'m-2 p-2 bg-amber-500'}
+                            onClick={() => showCategory('A')}
+                        >All</div>
+                        <div
+                            className={'m-2 p-2 bg-amber-500'}
+                            onClick={() => showCategory('C')}
+                        >Coffee</div>
+                        <div
+                            className={'m-2 p-2 bg-amber-500'}
+                            onClick={() => showCategory('D')}
+                        >Deserts</div>
+                        <div
+                            className={'m-2 p-2 bg-amber-500'}
+                            onClick={() => showCategory('G')}
+                        >Goods</div>
+                    </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+                        {menuLis}
+                    </div>
+                </div>
+    )
+
 }
 
 export default MenuComponent;
